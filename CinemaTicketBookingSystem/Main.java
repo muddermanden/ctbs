fe3baf9f8acf2ccdbfcb88675baebaca1a007cbc
Morten Henrichsen <muddermanden@gmail.com>
2015-10-28 11:11:47 +0100

    Refactor login feature into User class

--- CinemaTicketBookingSystem/Main.java
+++ CinemaTicketBookingSystem/Main.java
@@ -1,25 +1,22 @@
 package CinemaTicketBookingSystem;
 
-import java.util.ArrayList;
-
 public class Main
 {
 	private static MovieSchedule movieSchedule;
-	private static ArrayList<User> users = new ArrayList<User>();
 
 
 	/**
 	 * Starts the program.
 	 * 
 	 * @param args
-	 *            The arguments passed to the program. Takes no arguments.
+	 *        The arguments passed to the program. Takes no arguments.
 	 */
 	public static void main(String[] args)
 	{
 		// setup the application with mock data for demonstration purpose
 		setupMockData();
 
-		User user = promptLogin();
+		User user = User.login();
 		if (user instanceof Customer)
 		{
 			Booking myBooking = new Booking((Customer) user);
@@ -33,23 +30,6 @@ public class Main
 		{
 			System.out.println("Unknown username or password.");
 		}
-	}
-
-
-	public static User promptLogin()
-	{
-		String username = Utility.promptUserInputString("Type username:");
-		String password = Utility.promptUserInputString("Enter password:");
-
-		User match = null;
-		for (User user : users)
-		{
-			if (user.getUsername().equals(username) && user.getPassword().equals(password))
-			{
-				match = user;
-			}
-		}
-		return match;
 	}
 
 
@@ -70,7 +50,7 @@ public class Main
 		movieSchedule.addPresentationToSchedule(frida, 2015, 11, 25, 11, 30);
 		movieSchedule.addPresentationToSchedule(terminator1, 2015, 11, 26, 11, 30);
 
-		users.add(new Customer("John", "1234"));
-		users.add(new Employee("Jane", "abc"));
+		User.addUser(new Customer("John", "1234"));
+		User.addUser(new Employee("Jane", "abc"));
 	}
 }
--- CinemaTicketBookingSystem/User.java
+++ CinemaTicketBookingSystem/User.java
@@ -1,14 +1,68 @@
 package CinemaTicketBookingSystem;
 
+import java.util.ArrayList;
+
 public abstract class User
 {
-	private String username;
-	private String password;
+	private static ArrayList<User> users = new ArrayList<User>();
 
 
-	protected void setUsername(String username)
+	public static void addUser(User user)
 	{
+		users.add(user);
+	}
+
+
+	public static User login()
+	{
+		String username = Utility.promptUserInputString("Type username:");
+		String password = Utility.promptUserInputString("Enter password:");
+
+		User match = null;
+		for (User user : users)
+		{
+			if (user.getUsername().equals(username) && user.getPassword().equals(password))
+			{
+				match = user;
+			}
+		}
+		return match;
+	}
+
+	
+	// private fields
+	private String password;
+	private String username;
+
+
+	/**
+	 * Default constructor.
+	 */
+	protected User()
+	{
+		// empty
+	}
+
+
+	/**
+	 * Creates a user with username and password.
+	 * 
+	 * @param username
+	 *        The username the user is created with.
+	 * @param password
+	 *        The password the user is created with.
+	 */
+	protected User(String username, String password)
+	{
+		this(); // call the default constructor
 		this.username = username;
+		this.password = password;
+	}
+
+
+	protected String getPassword()
+	{
+		return this.password;
 	}
 
 
@@ -24,32 +78,9 @@ public abstract class User
 	}
 
 
-	protected String getPassword()
+	protected void setUsername(String username)
 	{
-		return this.password;
-	}
-
-
-	/**
-	 * Default constructor.
-	 */
-	protected User()
-	{
-		// empty
-	}
-
-
-	/**
-	 * Creates a user with username and password.
-	 * 
-	 * @param username The username the user is created with.
-	 * @param password The password the user is created with.
-	 */
-	protected User(String username, String password)
-	{
-		this(); // call the default constructor
 		this.username = username;
-		this.password = password;
 	}
 
 }
